4de970d4aaa7a2cfe913c66062b6d5984956bf69
Eduard Stromenko <estromenko@mail.ru>
2022-10-21 20:20:10 +0300

    Init project

---
+++ conf/docker.py
@@ -0,0 +1,24 @@
+import os
+
+from dotenv import load_dotenv
+
+from conf.settings import *  # type: ignore
+
+load_dotenv()
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DEBUG = os.getenv('DEBUG', 'false') == 'true'
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('PG_NAME', 'postgres'),
+        'USER': os.getenv('PG_USER', 'postgres'),
+        'PASSWORD': os.getenv('PG_PASSWORD', 'secret'),
+        'HOST': os.getenv('PG_HOST', 'localhost'),
+        'PORT': os.getenv('PG_PORT', '5432'),
+    }
+}
