d61c52b1bc18039a82836af13c3ad000b54fb8d1
Eduard Stromenko <estromenko@mail.ru>
2022-10-22 11:49:14 +0300

    Add swagger

--- conf/urls.py
+++ conf/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from conf.swagger import schema_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('exponents/', include('exponents.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
