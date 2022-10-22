7f18fe5dc492f084991dc323f6d8d7f1fe4242c9
Eduard Stromenko <estromenko@mail.ru>
2022-10-23 00:22:37 +0300

    Change pagination size

--- conf/docker.py
+++ conf/docker.py
@@ -34,7 +34,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 6,
+    'PAGE_SIZE': 8,
 }
 
 CORS_ALLOW_ALL_ORIGINS = DEBUG
