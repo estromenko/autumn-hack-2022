eb446f79d7c152e0fff78ac629782b51ffc88bc4
Eduard Stromenko <estromenko@mail.ru>
2022-10-22 22:13:48 +0300

    Add admin filter for exponents

--- exponents/admin.py
+++ exponents/admin.py
@@ -9,6 +9,11 @@ from exponents.models import (
 class ExponentAdmin(admin.ModelAdmin):
     """Админка экспонентов. """
 
+    list_filter = ('user__is_active', )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('user')
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
