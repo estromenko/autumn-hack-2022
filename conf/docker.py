ac016053b666ac4fb50179ecd748571c6df6e02a
Eduard Stromenko <estromenko@mail.ru>
2022-10-22 22:01:15 +0300

    Add pagination

--- conf/docker.py
+++ conf/docker.py
@@ -33,6 +33,8 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 6,
 }
 
 CORS_ALLOW_ALL_ORIGINS = DEBUG
